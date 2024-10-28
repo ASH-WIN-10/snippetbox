c7ecd9467c027c9b99ee7672cd715a6090031a7d
Ashvin <ashvinjangid10@gmail.com>
2024-10-28 20:50:19 +0530

    feat: dependency injection with closures/structs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,20 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	// Command line arguments
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		logger: logger,
+	}
 
 	// router
 	mux := http.NewServeMux()
@@ -18,15 +28,12 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	// structured logger
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	logger.Info("starting server", "addr", *addr)
-	logger.Info("started server", "link", "http://localhost:4000/")
 
 	err := http.ListenAndServe(*addr, mux)
 	logger.Error(err.Error())
