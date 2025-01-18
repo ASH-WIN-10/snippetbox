36f00aac425ac695a1c05909b3291c3d4a5eaae8
Ashvin <ashvinjangid10@gmail.com>
2025-01-18 21:32:21 +0530

    fix: send StatusUnprocessable code rather than StatusOK on finding a duplicate email

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -142,7 +142,7 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 
 			data := app.newTemplateData(r)
 			data.Form = form
-			app.render(w, r, http.StatusOK, "signup.tmpl.html", data)
+			app.render(w, r, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
 		} else {
 			app.serverError(w, r, err)
 		}
